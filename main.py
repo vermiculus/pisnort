e8548ceb927d0a6a50e36f0a22bc24c6edf8b875
Sean Allred <allred.sean@gmail.com>
2014-01-09 08:58:27 -0500

    Structure changes; remove police_s.wav

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ logging.config.fileConfig('logging.conf')
 def play_sound(relative_path):
     log = logging.getLogger('sounds')
     log.info('Playing sound: %s', relative_path)
-    subprocess.call(["aplay",  "sounds/{!s}".format(relative_path)])
+    subprocess.call(['aplay',  'sounds/{!s}'.format(relative_path)])
 
 if __name__ == '__main__':
     log = logging.getLogger('root')
     log.info('Execution started.')
 
     print 'Welcome to PiSnort!'
-    play_sound("welcome.wav")
+    play_sound('system/welcome.wav')
 
     state=object()
     state.testing = True
@@ -55,61 +55,49 @@ if __name__ == '__main__':
     log.info('Beginning main loop.')
     try:
         while True:
-            log = logging.getLogger('root')
             log.debug('Executing loop')
-              
             log.debug('Updating state...')
-              
-            # Check for changes in button state.  This logic should allow the
-            # system to manage a toggle on/off button for the system state
-            # using the momentary latch provided by the button.
             if gu.read_pin(pins['Button in']):
                 if not state.button_depressed:
                     log.debug('Caught button state change.')
                     state.button_depressed = True
             else:
                 state.button_depressed = False
-              
-            # Check for any input from the PIR.  If motion is detected, set
-            # the appropriate flag.  (This can easily be collapsed to one
-            # line, but is expanded here for clarity.)
             if gu.read_pin(pins['PIR Signal']):
                 log.info('Motion detected!')
                 state.motion_detected = True
             else:
                 state.motion_detected = False
-              
             log.debug('Updating state... Done.')
-            
             if state.button_depressed:
-                log = logging.getLogger('root')
                 log.debug('Toggling state...')
                   
                 if state.on:
                     log.debug('Turning LED OFF')
                     gu.set_pin(pins['LED power'], gpio.LOW)
-                    play_sound("deactivate.wav")
+                    play_sound('system/deactivate.wav')
                 else:
                     log.debug('Turning LED ON')
                     gu.set_pin(pins['LED power'], gpio.HIGH)
-                    play_sound("activate.wav")
+                    play_sound('system/activate.wav')
                   
                 state.on = not state.on
                 log.debug('Toggling state... Done.')
-              
             if state.testing:
                 for i in range(5):
                     print 'Toggling LED on pin {}'.format(pins['LED power'].number)
                     gu.toggle(pins['LED power'])
                     time.sleep(1)
-                print 'Playing siren'
-                play_sound("police_s.wav")
+                print 'Diagnostic complete.'
+                play_sound('system/diagnostic.wav')
             elif state.on and state.motion_detected:
                 choices = [snorts[key][0] for key in snorts]
                 soundfile = random.choice(choices)
                 log.info('Waiting ten seconds to play sound.')
                 time.sleep(10)
+                
                 play_sound(soundfile)
+                
                 log.info('Waiting five minutes to continue execution')
                 time.sleep(600)
     except KeyboardInterrupt:
